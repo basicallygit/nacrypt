fb4812f4479281ef3035095e4d3ebdcf7a80eb33
basicallygit <91993321+basicallygit@users.noreply.github.com>
2025-12-23 16:02:18 +0000

    Prefer clang-format-20

--- format.sh
+++ format.sh
@@ -2,10 +2,20 @@
 
 set -xe
 
+# Prefer clang-format-20 if it exists
+if command -v clang-format-20 >/dev/null 2>&1; then
+	CLANG_FORMAT="clang-format-20"
+elif command -v clang-format >/dev/null 2>&1; then
+	CLANG_FORMAT="clang-format"
+else
+	echo "No clang-format found in PATH" >&2
+	exit 1
+fi
+
 if [[ $1 == "--check" ]]; then
-	find . -type f \( -name "*.c" -o -name "*.h" \) -exec clang-format -style=file --dry-run -Werror {} +
+	find . -type f \( -name "*.c" -o -name "*.h" \) -exec $CLANG_FORMAT -style=file --dry-run -Werror {} +
 	exit 0
 fi
 
 # clang-format -style="{BasedOnStyle: LLVM, UseTab: Always, IndentWidth: 4, TabWidth: 4, PointerAlignment: Left, BreakBeforeBraces: Custom, BraceWrapping: {AfterControlStatement: MultiLine}}" -dump-config > .clang-format
-find . -type f \( -name "*.c" -o -name "*.h" \) -exec clang-format -style=file -i {} +
+find . -type f \( -name "*.c" -o -name "*.h" \) -exec $CLANG_FORMAT -style=file -i {} +
