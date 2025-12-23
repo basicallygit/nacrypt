2467cced0bddb4eef7b4c22033c984fe11e811d9
basicallygit <91993321+basicallygit@users.noreply.github.com>
2025-12-23 15:23:15 +0000

    Add --check option to check formatting

--- format.sh
+++ format.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
 set -xe
+
+if [[ $1 == "--check" ]]; then
+	find . -type f \( -name "*.c" -o -name "*.h" \) -exec clang-format -style=file --dry-run -Werror {} +
+	exit 0
+fi
+
 # clang-format -style="{BasedOnStyle: LLVM, UseTab: Always, IndentWidth: 4, TabWidth: 4, PointerAlignment: Left, BreakBeforeBraces: Custom, BraceWrapping: {AfterControlStatement: MultiLine}}" -dump-config > .clang-format
 find . -type f \( -name "*.c" -o -name "*.h" \) -exec clang-format -style=file -i {} +
