f6d1b44a226442aa6bc3905a2504915a7b00f695
basicallygit <91993321+basicallygit@users.noreply.github.com>
2026-01-02 00:15:21 +0000

    remove seccompfilter

--- include/seccompfilter.h
+++
@@ -1,8 +0,0 @@
-#ifndef NACRYPT_SECCOMP_FILTER_H
-#define NACRYPT_SECCOMP_FILTER_H
-
-#include <stdbool.h>
-
-bool apply_seccomp_filter(int input_fd, int output_fd);
-
-#endif // !defined(SECCOMP_FILTER_H)
